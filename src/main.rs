8778198dcd05836c3d19cefb94c9f576ff4750e2
RRValue <rrvalue@codeschnipsel.org>
2024-02-05 15:45:49 +0100

    apply second case, rename math to math_lingo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use math_lingo::*;
 
 fn main() {
-    println!("Evaluate equation \"one plus two\" = {}", math!(one plus two));
+    println!("Evaluate equation \"one plus two\" = {}", math_lingo!(one plus two));
+    println!("Evaluate equation \"one plus nine\" = {}", math_lingo!(one plus nine));
 }
