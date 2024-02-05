3bfbd3a423135cb5d023a55a1c55024d7b8c63b1
RRValue <rrvalue@codeschnipsel.org>
2024-02-05 16:32:35 +0100

    add some more expressions to test

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ use math_lingo::*;
 fn main() {
     println!("Evaluate equation \"one plus two\" = {}", math_lingo!(one plus two));
     println!("Evaluate equation \"one plus nine\" = {}", math_lingo!(one plus nine));
+    println!("Evaluate equation \"1 plus three\" = {}", math_lingo!(1 plus three));
+    println!("Evaluate equation \"nine plus 100\" = {}", math_lingo!(nine plus 100));
+    println!("Evaluate equation \"1.11 plus 2.22\" = {}", math_lingo!(1.11 plus 2.22));
 }
