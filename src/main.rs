988c5ddecbc79aec19c0ea8bdca7d08dcf7f9dd4
RRValue <rrvalue@codeschnipsel.org>
2024-02-06 11:02:23 +0100

    add tests

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,20 @@ fn main() {
     println!("Evaluate equation \"1 plus three\" = {}", math_lingo!(1 plus three));
     println!("Evaluate equation \"nine plus 100\" = {}", math_lingo!(nine plus 100));
     println!("Evaluate equation \"1.11 plus 2.22\" = {}", math_lingo!(1.11 plus 2.22));
+    println!("Evaluate equation \"1 through 3\" = {}", math_lingo!(1 through 3));
+}
+
+#[cfg(test)]
+mod tests {
+    use math_lingo::*;
+
+    #[test]
+    fn test_equations() {
+        assert_eq!(math_lingo!(one plus two), 3);
+        assert_eq!(math_lingo!(one plus nine), 10);
+        assert_eq!(math_lingo!(1 plus three), 4);
+        assert_eq!(math_lingo!(nine plus 100), 109);
+        assert_eq!(math_lingo!(1.11 plus 2.22), 3.33);
+        assert_eq!(math_lingo!(1 through 3), 1 / 3);
+    }
 }
